23e6ece9a1efa3830335a27abbed513283766e50
UnluckyKing233 <3303651496@qq.com>
2024-10-23 20:11:28 +0800

    完成作业

--- timer/timer.v
+++ timer/timer.v
@@ -1,11 +1,17 @@
-module timer(clk timer)                   // 每个时钟周期自增1
-	input clk;
-    output [7:0] timer;
-    reg [7:0] timer;
+module timer(                // 每个时钟周期自增1
+	input wire clk,
+    input wire rst,
+    output reg [7:0] timer;
+)
     
     always @ (posedge clk) 
     begin
-        timer <= tomer + 1;
+        if(rst)
+            timer <= 0;
+        else
+            count <= count + 1
+
+
     
     end
     
--- timer/timer_tb.v
+++ timer/timer_tb.v
@@ -5,3 +5,28 @@ testbench激励文件
 always #5 clk = ~clk;
 
 */
+
+`timescale 1ns / 1ps
+module timer_tb;
+    
+    reg clk;
+    reg rst;
+    wire [7:0] timer;
+
+    counter uut (
+        .clk(clk),
+        .rst(rst),
+        .count(count)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+
+        #100
+
+        $finish;
+    end
+
+endmodule
